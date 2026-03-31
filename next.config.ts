aec4ed95f5db38c011dc234035bcfc517c22a78a
Jason Webster <jasonwebster@Jasons-iMac.local>
2026-03-31 10:20:37 -0400

    Ensure sitemap.xml route is exposed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap.xml",
+      },
+    ];
+  },
 };
 
 export default nextConfig;

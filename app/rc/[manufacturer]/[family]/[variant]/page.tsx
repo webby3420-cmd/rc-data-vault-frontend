1299922896d334227c3db657b60704711fb42d38
webby3420-cmd <webby3420@gmail.com>
2026-03-31 12:31:48 -0400

    Update page.tsx

--- app/rc/[manufacturer]/[family]/[variant]/page.tsx
+++ app/rc/[manufacturer]/[family]/[variant]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import { VariantPage } from "@/components/variant-page/VariantPage";
 import { getVariantPagePayload } from "@/lib/variant-page";
